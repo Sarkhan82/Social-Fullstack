3e4f3bdfd6365ed96820ebb6aa58dab9e168a148
Sarkhan <david.jonquieres@gmail.com>
2022-06-14 20:43:08 +0200

    création serveur

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const userRoutes = require("./routes/user.routes");
+require("dotenv").config({ path: "./config/.env" });
+require("./config/db");
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// ROUTES
+app.use("api/user", userRoutes);
+
+// SERVER
+app.listen(process.env.PORT, () => {
+  console.log(`Listening on port ${process.env.PORT}`);
+});
